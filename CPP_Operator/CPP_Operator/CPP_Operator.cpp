f26331f9eb8af5a5910f1d39c589ea5b2ca3fb85
Huxley <D27Huxley@gmail.com>
2025-05-29 19:47:06 +0900

    Initial commit

---
+++ CPP_Operator/CPP_Operator/CPP_Operator.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+int a = 1;
+int b = 2;
+
+bool isSame;
+bool isDifferent;
+bool isGreater;
+bool isSmaller;
+
+bool test;
+
+int hp = 100;
+bool isInvincible = true;
+
+int main()
+{
+#pragma region 비교 연산
+
+	// 언제 필요할까?
+	// ex) 체력이 0이 되면 사망
+	// ex) 체력이 30% 이하이면 궁극기를 발동 (100 * hp / maxHP)
+	// ex) 경험치가 100이상이면 레벨업
+
+	// a == b : a와 b의 값이 같은가?
+	// 같으면 1, 다르면 0 반환
+	isSame = (a == b);
+
+	// a != b : a 와 b의 값이 다른가?
+	// 다르면 1, 같으면 0 반환
+	isDifferent = (a != b);
+
+	// a > b : a가 b보다 큰가?
+	// a >= b : a가 b보다 크거나 같은가?
+	// 1 아니면 0
+	isGreater = (a > b);
+
+	// a < b : a가 b보다 작은가?
+	// a <= b : a가 b보다 작거나 같은가?
+	// 1 아니면 0
+	isSmaller = (a < b);
+
+
+
+#pragma endregion
+#pragma region 논리 연산
+	// 언제 필요할까? 조건에 대한 논리적 사고가 필요할 때
+	// ex) 로그인할 때 아이디도 일치하고 AND 비밀번호도 일치해야 한다.
+	// ex) 길드 마스터이거나 OR 운영자 계정이면 길드 해산 가능
+
+	// ! not 
+	// 0이면 1 반환 그 외엔 0
+	test = !isSame; // 사실상 isDifferent의 의미
+
+	// %% and
+	// a && b : 둘 다 1이면 1, 그 외 0
+	test = (hp <= 0 && isInvincible == false); // 죽음 (체력0이고 무적상태 아닐 경우)
+
+	// || or
+	// a || b : 둘 중 하나라도 1이면 1 (둘 다 0이면 0)
+	test = (hp > 0 || isInvincible == true); // 생존 (체력이 0 보다 크거나 무적상태일 경우)
+
+#pragma endregion
+}
---
+++ CPP_String/CPP_String/CPP_String.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+// 문자와 문자열
+// bool은 그냥 정수지만, 참/거짓을 나타내기 위해 사용
+// 사실char도 마찬가지로 쓰는데 그냥 정수지만 '문자' 의미를 나타내기 위해서 사용
+
+// char : 알파벳 / 숫자 문자를 나타냄
+// wchar_t : 유니코드 문자를 나타냄
+
+// ASCII (American Standard Code for Information Interchange)
+
+// '문자'의 의미로 작은 따옴표 '' 사용
+
+char ch = 97; // a 아스키코드 97
+char ch2 = '2'; // 2 2에 해당하는 아스키코드 값이 저장
+char ch3 = '1' + 1; // 2 1에 해당하는 아스키코드 값이 저장되고 거기에 +1
+
+// 국제화 시대에 영어만으로 서비스 할 수 없음
+// 전 세계 모든 문자에 대해 유일 코드를 부여한 것이 유니코드(unicode)
+// 참고) 유니코드에서 가장 많은 번호를 차지하는게 한국어/중국어
+
+// 유니코드는 표기 방식이 여러가지가 있는데 대표적으로 UTF8 / UTF16
+// UTF8
+// - 알파벳, 숫자 1바이트 (ASCII 동일한 번호)
+// - 유럽 지역의 문자는 2바이트
+// - 한글, 한자 등 3바이트
+// UTF16
+// - 알파벳, 숫자, 한글, 한자 등 거의 대부분 문자 2바이트
+// - 매우 예외적인 고대 문자만 4바이트 (사실상 없음)
+
+wchar_t wch = L'안';
+
+
+// Escape Sequence
+// 표기하기 애매한 애들을 표현
+// \0 = 아스키코드0 = Null
+// \t = 아스키코드9 = Tab
+// \n = 아스키코드10 = LineFeed (한 줄 아래로)
+// \r = 아스키코드13 = CarriageReturn (커서<<)
+// \' = '
+
+// 문자열
+// 문자들이 열을 지어서 모여 있는 것(문자 배열)
+// 정수 (1~8바이트) 고정 길이로
+// 끝은 NULL (0)
+
+// char str[] = { 'h', 'e', 'l', 'l', 'o' };
+
+char str2[] = "Hello World";
+
+int main()
+{
+    char str[] = { 'h', 'e', 'l', 'l', 'o', '\0'};
+
+    cout << "체력이" << ch << "남았습니다." << endl;
+    cout << "체력이" << ch2 << "남았습니다." << endl;
+    cout << "체력이" << ch3 << "남았습니다." << endl;
+
+    // cout는 char 전용
+
+    wcout.imbue(locale("kor"));
+    wcout << wch << endl;
+
+    cout << str << endl;
+    cout << str2 << endl;
+}
